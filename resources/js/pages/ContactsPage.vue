78341fdf9845552d5f940d9a20ff2c8189e644eb
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-10-20 15:15:41 +0200

    add: creazione di una tabella con relativo Model per le richieste + creazione di un nuovo oggetto mail + creazione della vista in blade + Nella pagina ContactsPage (.vue) -> chiamata api post e creazione del form per mandare un email

--- resources/js/pages/ContactsPage.vue
+++ resources/js/pages/ContactsPage.vue
@@ -1,12 +1,41 @@
 <template>
     <div class="container mt-5">
-        <h1>Contatti</h1>
+        <h1>Contattaci:</h1>
+
+        <form>
+            <label for="name">Nome</label>
+            <input type="text" id="name" name="name" v-model="name">
+            
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" v-model="email">
+            
+            <label for="message">Messaggio</label>
+            <input type="text" id="message" name="message" v-model="message">
+        </form>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'ContactsPage'
+    name: 'ContactsPage',
+    data() {
+        return {
+            name: '',
+            email: '',
+            message: ''
+        }
+    },
+    methods: {
+        sendEmail() {
+            axios.post('/api/contacts', {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }).then(response => {
+                console.log(response);
+            });
+        }
+    }
 }
 </script>
 
---
+++ resources/views/emails/new-email.blade.php
@@ -0,0 +1,8 @@
+<h1>Ciao amministratore</h1>
+<p>
+    Hai ricevuto una nuova mail. <br>
+    Dettagli: <br>
+    Nome: {{$lead->name}}
+    E-mail: {{$lead->email}}
+    Messaggio: {{$lead->message}}
+</p>
